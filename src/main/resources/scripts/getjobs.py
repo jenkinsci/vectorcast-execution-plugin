bf577e80f2a6237751a138e2a9b2e68381788957
TimSVector <tim.schneider@vector.com>
2020-02-19 16:54:23 -0500

    Help file for single checkout

--- src/main/resources/scripts/getjobs.py
+++ src/main/resources/scripts/getjobs.py
@@ -3,16 +3,22 @@ import os
 import re
 import sys
 
+try:
+    from vector.apps.DataApi.vcproject_api import VCProjectApi
+    useManageApi = True
+except:
+    useManageApi = False
+
 manageCMD=os.environ['VECTORCAST_DIR'] + "/manage"
 
 
-def printEnvironmentInfo(ManageProjectName):
+def parseFullStatusReport(ManageProjectName):
     p = subprocess.Popen(manageCMD + " --project " + ManageProjectName + " --full-status",shell=True,stdout=subprocess.PIPE)
     out, err = p.communicate()
     
     job_list = []
     level_list = []
-
+    info = ""
     list = out.split(os.linesep)
 
     for str in list:
@@ -21,10 +27,21 @@ def printEnvironmentInfo(ManageProjectName):
         elif re.match("^    [^\s]",str) is not None:
             testsuite = str.split()[0]
         elif re.match("^      [^\s]",str) is not None and not str.startswith("      Disabled Environment"):
-                
             env_name = str.split()[0]
-                
-            print ("%s %s %s" % (compiler , testsuite , env_name))
+            info += ("%s %s %s\n" % (compiler , testsuite , env_name))
+    return info
 
+def printEnvironmentInfo(ManageProjectName):
+    if useManageApi:
+        info = ""
+        m_api = VCProjectApi(ManageProjectName)
+        for env in m_api.Environment.all():
+            if env.is_enabled:
+                info += ("%s %s %s\n" % (env.compiler.name, env.testsuite.name, env.name))
+    else:
+        info = parseFullStatusReport(ManageProjectName)
+        
+    print info
+    
 if __name__ == "__main__":
     printEnvironmentInfo(sys.argv[1])
